831045f206b00ff8d82215a927c94de3dba751d8
Dominic Montelongo and Stuart McKay <stuart@>
2019-03-27 13:58:56 -0700

    Add css and js modifications

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,41 @@
+// back-end logic
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result1 = add(number1, number2);
-alert(result1);
-
 var subtract = function(number1, number2) {
   return number1 - number2;
-}
-
-var result2 = subtract(number1, number2);
-alert(result2);
+};
 
 var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
-var result3 = multiply(number1, number2);
-alert(result3);
-
 var divide = function(number1, number2) {
   return number1 / number2;
-}
+};
 
-var result4 = divide(number1, number2);
-alert(result4);
+// Everything below this line is user interface (front-end) logic
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
+
+// var result1 = add(number1, number2);
+
+
+// var result2 = subtract(number1, number2);
+// alert(result2);
+//
+// var result3 = multiply(number1, number2);
+// alert(result3);
+//
+// var result4 = divide(number1, number2);
+// alert(result4);
